6bd6ed8882db7c25fa73109b7f722513e27dc1ef
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-12 14:03:40 +0900

    created Lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/lists', to: 'lists#index', as: :lists
+  get '/list/:id', to: 'lists#show', as: :list
+  get '/lists/new', to: 'lists#new', as: :new_list
+  post '/lists/new', to: 'lists#create'
 end
