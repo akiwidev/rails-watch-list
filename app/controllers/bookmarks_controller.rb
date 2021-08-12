678d4a5321676763657062ffcb491c52bbd478d9
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-12 15:40:50 +0900

    created new action for bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,15 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path(@bookmark.lists)
   end
 end
