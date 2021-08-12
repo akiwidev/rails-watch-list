17dda0ae92020ee748c0c8415b73f84283944323
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-12 15:20:15 +0900

    created bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/lists', to: 'lists#index', as: :lists
-  get '/lists/new', to: 'lists#new', as: :new_list
-  post '/lists', to: 'lists#create'
-  get '/list/:id', to: 'lists#show', as: :list
+  # get '/lists', to: 'lists#index', as: :lists
+  # get '/lists/new', to: 'lists#new', as: :new_list
+  # post '/lists', to: 'lists#create'
+  # get '/list/:id', to: 'lists#show', as: :list
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
